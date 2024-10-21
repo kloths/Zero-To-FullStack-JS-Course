80dd12580e90c9517e396e1efab85c4fc0eb17fa
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 00:58:19 +0100

    Exercise 3 Complete

--- 03. DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
+++ 03. DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,12 @@
 // innerText
 // textContent
 // innerHTML
+
+// const p = document.querySelector('p');
+// console.log(p.innerText);
+// console.log(p.textContent);
+// console.log(p.innerHTML);
+
+const h1 = document.querySelector("h1");
+// h1.innerText = "Text Changed";
+h1.innerHTML = "<strong>Text Changed</strong>";
