f44dc7a8d20accdb5ea55cb2e90dfab25fead499
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 00:37:57 +0100

    Exercise 2 Complete

--- 03. DOM/Starter/2. Accessnig the DOM/exercise/app.js
+++ 03. DOM/Starter/2. Accessnig the DOM/exercise/app.js
@@ -3,3 +3,15 @@
 // 3. Select div with the ID of blue which holds the text "Blue" by using getElementsByClassName
 // 4. Select div which has the class & Id of "yello" by using querySelector()
 // 5. Select all the elements which has the class of "teal" by using querySelectorAll
+
+const h4 = document.getElementsByTagName("h4")[0];
+const greenDiv = document.getElementsByClassName("green");
+const blueDiv = document.getElementById("blue");
+const yellowDiv = document.querySelector("#yellow.yellow");
+const teal = document.querySelectorAll(".teal");
+
+console.log(h4);
+console.log(greenDiv);
+console.log(blueDiv);
+console.log(yellowDiv);
+console.log(teal);
