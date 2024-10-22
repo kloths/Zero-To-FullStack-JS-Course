052024ef1aaecede58d1044abd426183e131c1f3
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 15:41:55 +0100

    Simple-calculator

--- 03. DOM/Projects/5. Simple-calculator/app.js
+++ 03. DOM/Projects/5. Simple-calculator/app.js
@@ -1,21 +1,24 @@
-document.getElementById("btn").addEventListener("click", () => {
-  // Getting Elements From The DOM
-  let num1 = document.querySelector(".num1").value;
-  let num2 = document.querySelector(".num2").value;
-  let result = document.querySelector(".result");
-  let oprator = document.getElementById("selectOp").value;
+const result = document.querySelector(".result");
+const btn = document.querySelector("#btn");
 
-  switch (oprator) {
+btn.addEventListener("click", () => {
+  const num1 = parseFloat(document.querySelector(".num1").value);
+  const num2 = parseFloat(document.querySelector(".num2").value);
+  const operator = document.querySelector("#selectOp").value;
+  switch (operator) {
     case "plus":
-      result.innerHTML = +num1 + +num2;
+      result.innerHTML = num1 + num2;
       break;
     case "min":
-      result.innerHTML = +num1 - +num2;
+      result.innerHTML = num1 - num2;
       break;
     case "dev":
-      result.innerHTML = +num1 / +num2;
+      result.innerHTML = num1 / num2;
       break;
     case "multi":
-      result.innerHTML = +num1 * +num2;
+      result.innerHTML = num1 * num2;
+      break;
+    default:
+      break;
   }
 });
