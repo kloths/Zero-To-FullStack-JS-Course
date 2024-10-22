6aa2d4b2bd2ccbf24725bdfdf649e90aef08d2af
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 04:23:46 +0100

    3 ways to add events

--- 03. DOM/Starter/9. 3 ways to add events/app.js
+++ 03. DOM/Starter/9. 3 ways to add events/app.js
@@ -1,5 +1,15 @@
 // ----------- BAD WAY-----------
+const secondBtn = document.querySelector(".second-btn");
+secondBtn.onclick = function () {
+  alert("bad");
+};
 
 // ----------- GREAT WAY-----------
+const best = document.querySelector(".best");
+best.addEventListener("click", () => alert("best"));
 
 // ----------- Event (e) Object -----------
+const para = document.querySelector(".para");
+para.addEventListener("click", (e) => {
+    console.log(e);
+})
