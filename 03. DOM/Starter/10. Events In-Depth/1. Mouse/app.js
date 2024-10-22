cd3613b6041b28702e7abd84580c6844d213cb9f
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 06:21:50 +0100

    Events In-Depth

--- 03. DOM/Starter/10. Events In-Depth/1. Mouse/app.js
+++ 03. DOM/Starter/10. Events In-Depth/1. Mouse/app.js
@@ -21,3 +21,53 @@ let seven = document.querySelector(".mousemove");
 let eight = document.querySelector(".mouseout");
 let nine = document.querySelector(".mouseover");
 let ten = document.querySelector(".mouseup");
+
+one.addEventListener("click", (e) => {
+  console.log("Clicked");
+  console.log(e);
+});
+
+two.addEventListener("contextmenu", (e) => {
+  console.log("Open context menu");
+  console.log(e);
+});
+
+three.addEventListener("dblclick", (e) => {
+  console.log("Double clicked");
+  console.log(e);
+});
+
+four.addEventListener("mousedown", (e) => {
+  console.log("Mouse button pressed");
+  console.log(e);
+});
+
+five.addEventListener("mouseenter", (e) => {
+  console.log("Mouse entered");
+  console.log(e);
+});
+
+six.addEventListener("mouseleave", (e) => {
+  console.log("Mouse left");
+  console.log(e);
+});
+
+seven.addEventListener("mousemove", (e) => {
+  console.log("Mouse moved");
+  console.log(e);
+});
+
+eight.addEventListener("mouseout", (e) => {
+  console.log("Mouse left the element");
+  console.log(e);
+});
+
+nine.addEventListener("mouseover", (e) => {
+  console.log("Mouse entered the element");
+  console.log(e);
+});
+
+ten.addEventListener("mouseup", (e) => {
+  console.log("Mouse button released");
+  console.log(e);
+});
--- 03. DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
+++ 03. DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,21 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+
+// input.addEventListener("keypress", () => {
+//   console.log("Key pressed");
+// });
+
+// input.addEventListener("keydown", () => {
+//   console.log("Key down");
+// });
+
+// input.addEventListener("keyup", () => {
+//   console.log("Key up");
+// });
+
+// USEFUL PROPERTIES & METHODS
+input.addEventListener("keydown", (e) => {
+  console.log(e.code);
+  console.log(e.ctrlKey);
+  console.log(e.shiftKey);
+});
