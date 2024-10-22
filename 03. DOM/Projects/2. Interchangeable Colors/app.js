9e5d9f13646102508cc62ad341d949cf64712813
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 13:52:13 +0100

    Interchangable Colors

--- 03. DOM/Projects/2. Interchangeable Colors/app.js
+++ 03. DOM/Projects/2. Interchangeable Colors/app.js
@@ -1,40 +1,16 @@
-// Getting Elements From The DOM
+const container = document.querySelector(".content-container");
 const btns = document.querySelectorAll(".btn");
-const body = document.body;
-// console.log(btns);
 
-// Iterating over all buttons
+const colors = new Set();
+
 btns.forEach((btn) => {
+  colors.add(btn.value);
   btn.addEventListener("click", () => {
-    number = btn.value;
-    changeBackground(number);
+    const containsColor = container.classList.contains(btn.value);
+    container.classList.remove(...colors);
+    if (containsColor) return;
+    container.classList.toggle(btn.value);
   });
 });
 
-function changeBackground(number) {
-  // console.log(number);
-  body.className = "";
-
-  switch (number) {
-    case "purple":
-      body.classList.add("purple");
-      break;
-    case "blue":
-      body.classList.add("blue");
-      break;
-    case "red":
-      body.classList.add("red");
-      break;
-    case "green":
-      body.classList.add("green");
-      break;
-    case "yellow":
-      body.classList.add("yellow");
-      break;
-    case "teal":
-      body.classList.add("teal");
-      break;
-    default:
-      break;
-  }
-}
+console.log(colors);
