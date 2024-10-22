0737e4aad4030de0360f07fd8aba44d11811233e
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 01:19:28 +0100

    Exercise 5 Complete

--- 03. DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
+++ 03. DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
@@ -2,3 +2,13 @@
 // 2. Use getAttribute(attrName) to check the attribute.
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+const a = document.querySelector("a");
+console.log(a.getAttribute("href"));
+
+const aTwo = document.querySelector(".a-two");
+aTwo.setAttribute(
+  "href",
+  "https://www.udemy.com/course/the-complete-javascript-course/"
+);
+console.log(aTwo);
--- 03. DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ 03. DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -3,3 +3,24 @@
 // type
 // getAttribute(attrName)
 // setAttribute(attrName, value)
+
+const a = document.querySelector("a");
+const input = document.querySelector("input");
+
+// Getting attributes
+// console.log(a.href);
+// console.log(input.value);
+// console.log(input.type);
+
+// Setting attributes
+// a.href = "https://www.example.com";
+// console.log(input.value = "Hi");
+// console.log(input.type = "password");
+// console.log(input.placeholder = "Enter your password");
+
+console.log(input.getAttribute("type"));
+console.log(a.getAttribute("href"));
+
+a.setAttribute("href", "https://www.example.com");
+input.setAttribute("type", "password");
+input.setAttribute("placeholder", "Enter your password");
