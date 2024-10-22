4020f981b9c0de3ed9b8fea4f076c1a3cfe46a1c
klothes <115721449+kloths@users.noreply.github.com>
2024-10-22 01:30:17 +0100

    Siblings

---
+++ 03. DOM/Starter/6. Siblings/app.js
@@ -0,0 +1,29 @@
+// const firstLi = document.querySelector("li");
+
+// console.log(firstLi.parentElement);
+// console.log(firstLi.parentElement.parentElement);
+// console.log(firstLi.parentElement.parentElement.parentElement);
+// console.log(firstLi.parentElement.parentElement.parentElement.parentElement);
+
+// const ul = document.querySelector("ul");
+// console.log(ul.children[0]);
+// console.log(ul.children[1]);
+// console.log(ul.children[2]);
+// console.log(ul.children[3]);
+// console.log(ul.children[0].innerHTML = "one");
+
+// console.log(firstLi.nextElementSibling.textContent);
+// console.log(firstLi.nextElementSibling.nextElementSibling.textContent);
+// console.log(firstLi.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
+// console.log(firstLi.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling);
+
+const fourthLi = document.querySelector(".fourth");
+console.log(fourthLi.previousElementSibling);
+console.log(fourthLi.previousElementSibling.previousElementSibling);
+console.log(
+  fourthLi.previousElementSibling.previousElementSibling.previousElementSibling
+);
+console.log(
+  fourthLi.previousElementSibling.previousElementSibling.previousElementSibling
+    .previousElementSibling
+);
